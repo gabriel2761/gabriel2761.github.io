34083f1313d74871a0a0107f108199ac8301ab35
Gabriel Mojica <niko.mojica@live.com>
2016-07-16 01:46:51 +1000

    chore: Add gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var inlinesource = require('gulp-inline-source');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('default', function() {
     console.log('test');
@@ -11,4 +12,10 @@ gulp.task('minify', function() {
         .pipe(inlinesource())
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'));
 });
